4d793729bdb74ddffee1c3dc4e74fb002e0a226e
Deynjiru254 <deynjiru@gmail.com>
2022-07-05 10:42:35 +0300

    My commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - prints name of compilation file with new file
+ * main - prints name of compilation file with new line
  * Return: 0
- * */
+ **/
 int main(void)
 {
 	printf("%s\n", _BASE_FILE_);
